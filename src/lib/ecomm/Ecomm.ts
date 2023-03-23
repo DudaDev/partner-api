97f1925a83979e3264d2f7c2eb6f5bccd8ca08bf
Jason Hummel <jason.hummel@duda.co>
2023-03-23 13:44:51 -0600

    Adds update ecomm settings endpoint
    * Adds a new method
    * Adds new types
    * Adds test for new method
    
    Closes #68

--- src/lib/ecomm/Ecomm.ts
+++ src/lib/ecomm/Ecomm.ts
@@ -1,11 +1,21 @@
 import Resource from '../base';
 import Products from './Products';
 import Gateways from './Gateways';
+import * as Types from './types';
+import { APIEndpoint } from '../APIEndpoint';
 
 class Ecomm extends Resource {
   gateways = new Gateways(this.config);
 
   products = new Products(this.config);
+
+  update = APIEndpoint<Types.UpdateEcommPayload, Types.UpdateEcommResponse>({
+    method: 'patch',
+    path: '/api/sites/multiscreen/{site_name}/ecommerce',
+    defaults: {
+      host: 'api.duda.co',
+    },
+  });
 }
 
 export default Ecomm;
