7721dee1210e44914932a9d5730e7ef7781279d4
Pay Estakhri <paymon.estakhri@duda.co>
2023-10-24 15:34:55 -0700

    Site Payloads/Responses - Updated Get Site Response - Updated Create/Update Site Payloads

--- src/lib/sites/types.ts
+++ src/lib/sites/types.ts
@@ -70,15 +70,23 @@ export interface SiteLabel {
   name: string
 }
 
+export interface Opentable {
+  restaurant_id?: string,
+  country?: string,
+  location?: string
+}
+
+export interface Agendize {
+  company_id?: string,
+  account_id?: string
+}
+
 export interface Site {
-  account_name?: string,
-  fav_icon?: string,
   external_uid?: string,
+  piwik_id?: string,
   google_tracking_id?: string,
   googletagmanager_container_id?: Array<string>,
-  additionalLanguages?: Array<Languages>,
   site_domain?: string,
-  lang?: string,
   site_business_info?: {
     business_name?: string,
     address?: {
@@ -90,7 +98,8 @@ export interface Site {
     },
     phone_number?: string,
     email?: string,
-    opentable_info?: Array<any>
+    opentable_info?: Array<Opentable>,
+    agendize_info?: Array<Agendize>
   },
   site_alternate_domains?: {
     domains?: Array<string>,
@@ -109,25 +118,7 @@ export interface Site {
       missing_required_fields?: Array<string>,
       missing_recommended_fields?: Array<string>
     }
-  },
-  site_name?: string,
-  template_id?: number,
-  site_default_domain?: string,
-  preview_site_url?: string,
-  overview_site_url?: string,
-  editor_site_url?: string,
-  last_published_date?: string,
-  first_published_date?: string,
-  last_reset_by?: string,
-  certificate_status?: 'COMPLETE' | 'IN_PROGRESS' | 'FAILED',
-  modification_date?: string,
-  creation_date?: string,
-  publish_status?: 'PUBLISHED' | 'UNPUBLISHED' | 'NOT_PUBLISHED_YET',
-  thumbnail_url?: string,
-  canonical_url?: string,
-  store_status?: 'NONE' | 'ACTIVE' | 'SUSPENDED',
-  store_type?: 'NATIVE' | 'THIRDPARTY',
-  labels?: Array<SiteLabel>
+  }
 }
 
 export type SiteThemeColor = {
@@ -149,7 +140,31 @@ export interface UpdateSiteThemePayload {
   colors: Array<SiteThemeColor>,
 }
 
-export type GetSiteResponse = Site;
+export interface GetSiteResponse extends Site {
+  site_name: string,
+  site_default_domain?: string,
+  preview_site_url?: string,
+  overview_site_url?: string,
+  editor_site_url?: string,
+  last_published_date?: string,
+  first_published_date?: string,
+  force_https?: boolean,
+  additionalLanguages?: Array<Languages>,
+  fav_icon?: string,
+  last_reset_by?: string,
+  certificate_status?: 'COMPLETE' | 'IN_PROGRESS' | 'FAILED',
+  modification_date?: string,
+  creation_date?: string,
+  publish_status?: 'PUBLISHED' | 'UNPUBLISHED' | 'NOT_PUBLISHED_YET',
+  thumbnail_url?: string,
+  canonical_url?: string,
+  store_status?: 'NONE' | 'ACTIVE' | 'SUSPENDED',
+  store_type?: 'NATIVE' | 'THIRDPARTY',
+  cookie_notification?: string,
+  lang?: string,
+  labels?: Array<SiteLabel>
+}
+
 export type CreateSiteResponse = SiteNamedResponse;
 export type DuplicateSiteResponse = SiteNamedResponse;
 
@@ -199,27 +214,11 @@ type FirstPartialUpdateSitePayload = {
   site_name: string
 }
 
-type SecondPartialUpdateSitePayload = {
-  site_domain: string,
-  default_domain_prefix: string,
-  external_uid: string,
-  lang: string,
-  site_alternate_domains: {
-    domains?: Array<string>,
-    is_redirect?: boolean
-  },
-  force_https: boolean,
-  site_seo: {
-    og_image?: string,
-    title?: string,
-    description?: string
-  },
-  schemas: {
-    local_business?: {
-      enabled?: boolean
-    }
-  },
-  fav_icon: string,
+interface SecondPartialUpdateSitePayload extends Site {
+  fav_icon?: string,
+  force_https?: boolean,
+  cookie_notification?: string,
+  default_domain_prefix?: string
 }
 
 type AtLeastOne<T, U = {[K in keyof T]: Pick<T, K> }> = Partial<T> & U[keyof U]
@@ -235,45 +234,13 @@ export type GetSiteByExtIDPayload = {
   external_uid: string,
 }
 
-export interface CreateSitePayload {
-  template_id: number | string,
-  fav_icon?: string,
+export interface CreateSitePayload extends Site {
+  template_id: string | number,
+  url?: string,
   default_domain_prefix?: string,
+  labels?: Array<SiteLabel>,
   lang?: string,
-  site_data?: {
-    external_uid?: string,
-    site_domain?: string,
-    site_alternate_domains?: {
-      domains?: Array<string>,
-      is_redirect?: boolean
-    },
-    site_seo?: {
-      og_image?: string,
-      title?: string,
-      description?: string
-    },
-    site_business_info?: {
-      business_name?: string,
-      address?: {
-        street?: string,
-        city?: string,
-        state?: string,
-        country?: string,
-        zip_code?: string
-      },
-      phone_number?: string,
-      email?: string,
-      opentable_info?: Array<any>
-    },
-    schemas?: {
-      local_business?: {
-        enabled?: boolean,
-        status?: 'MISSING_REQUIRED_FIELDS' | 'MISSING_RECOMMENDED_FIELDS' | 'VALID',
-        missing_required_fields?: Array<string>,
-        missing_recommended_fields?: Array<string>
-      }
-    },
-  }
+  site_data?: Site
 }
 
 export type GetSiteThemePayload = {
