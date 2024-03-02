83df3fab0d6821417e7f4f8aeb603b4a12b6b2c5
Jason Hummel <jason.hummel@duda.co>
2024-03-01 23:08:32 -0700

    adds legacy refresh method to token subclass

--- src/lib/apps/tokens/Tokens.ts
+++ src/lib/apps/tokens/Tokens.ts
@@ -2,20 +2,24 @@ import * as Types from './types';
 import { SubResource } from '../../base';
 import { APIEndpoint } from '../../APIEndpoint';
 
+const template = {
+  method: 'post' as const,
+  path: '/{app_uuid}/token/refresh',
+  defaults: {
+    host: 'api.duda.co',
+  },
+  bodyParams: {
+    refresh_token: {
+      type: 'string' as const,
+      required: true,
+    },
+  },
+};
+
 class AppsTokens extends SubResource {
-  create = APIEndpoint<Types.RefreshTokenPayload, Types.RefreshTokenResponse>({
-    method: 'post',
-    path: '/{app_uuid}/token/refresh',
-    defaults: {
-      host: 'api.duda.co',
-    },
-    bodyParams: {
-      refresh_token: {
-        type: 'string',
-        required: true,
-      },
-    },
-  });
+  create = APIEndpoint<Types.RefreshTokenPayload, Types.RefreshTokenResponse>(template);
+
+  refresh = APIEndpoint<Types.RefreshTokenPayload, Types.RefreshTokenResponse>(template);
 }
 
 export default AppsTokens;
--- tests/app/token.tests.ts
+++ tests/app/token.tests.ts
@@ -1,21 +1,21 @@
-import nock from "nock"
-import { expect } from "chai"
-import { Duda } from "../../src/index"
+import nock from 'nock';
+import { expect } from 'chai';
+import { Duda } from '../../src/index';
 
 describe('App store token tests', () => {
-  const base_path = '/api/integrationhub/application' 
-  const user = 'testuser'
-  const pass = 'testpass'
+  const base_path = '/api/integrationhub/application';
+  const user = 'testuser';
+  const pass = 'testpass';
   const encoded_auth = Buffer.from(`${user}:${pass}`).toString('base64');
-  const app_uuid = 'uuid'
-  const refresh_token = '123456'
+  const app_uuid = 'uuid';
+  const refresh_token = '123456';
 
   const response = {
-    type: "bearer",
-    authorization_code: "XXX-XXXXX-XXXXX",
-    refresh_token: "YYY-YYYYY-YYYYY",
-    expiration_date: 1555550616864
-  }
+    type: 'bearer',
+    authorization_code: 'XXX-XXXXX-XXXXX',
+    refresh_token: 'YYY-YYYYY-YYYYY',
+    expiration_date: 1555550616864,
+  };
 
   let duda: Duda;
   let scope: nock.Scope;
@@ -25,21 +25,30 @@ describe('App store token tests', () => {
       user,
       pass,
       env: Duda.Envs.direct,
-    })
+    });
 
     scope = nock(`https://${Duda.Envs.direct}`, {
       reqheaders: {
-        'authorization': `Basic ${encoded_auth}`,
-      }
-    })
-  })
+        authorization: `Basic ${encoded_auth}`,
+      },
+    });
+  });
 
   it('can create a new access token', async () => {
     scope.post(`${base_path}/${app_uuid}/token/refresh`, (body) => {
-      expect(body).to.eql({ refresh_token: refresh_token });
+      expect(body).to.eql({ refresh_token });
       return body;
-    }).reply(200, response)
+    }).reply(200, response);
 
     return await duda.appstore.tokens.create({ app_uuid, refresh_token });
-  })
-})
+  });
+
+  it('can use refresh to create a new access token', async () => {
+    scope.post(`${base_path}/${app_uuid}/token/refresh`, (body) => {
+      expect(body).to.eql({ refresh_token });
+      return body;
+    }).reply(200, response);
+
+    return await duda.appstore.tokens.refresh({ app_uuid, refresh_token });
+  });
+});
