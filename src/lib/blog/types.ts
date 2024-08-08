11729a1a690ec69bccbddbf2f99684f497ce2d43
Pay Estakhri <paymon.estakhri@duda.co>
2024-08-08 12:37:37 -0700

    Blog Endpoints - Added Blog Endpoints - Created new blog folder with accompanying types.ts file for new Blog Endpoints - Created corresponding blog.tests.ts file for Blog Endpoints - Updated README

---
+++ src/lib/blog/types.ts
@@ -0,0 +1,40 @@
+export interface ImportBlog {
+    feed_url: string,
+    import_type: 'APPEND' | 'OVERWRITE'
+}
+
+export interface ImportBlogPayload extends ImportBlog {
+    site_name: string
+}
+
+export interface ImportBlogResponse extends ImportBlog {}
+
+export interface Thumbnail {
+    url: string
+}
+
+export interface MainImage {
+    url: string
+}
+
+export interface ImportBlogPost {
+    title: string,
+    description: string,
+    content: string,
+    author: string,
+    thumbnail: Thumbnail,
+    main_image: MainImage
+}
+
+export interface ImportBlogPostPayload extends ImportBlogPost {
+    site_name: string
+}
+
+export interface ImportBlogPostResponse extends ImportBlogPost {}
+
+export interface DeleteBlogPayload {
+    site_name: string,
+    delete_backups: boolean
+}
+
+export type DeleteBlogResponse = void;
