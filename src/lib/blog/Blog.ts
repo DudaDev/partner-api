fbc096dfe341e547545e016d78c4fd3b6b638b81
Pay Estakhri <paymon.estakhri@duda.co>
2025-01-24 08:16:01 -0800

    Blog API Endpoints (#228)
    - Added new Blog API Endpoints
    - Updated Blog types file to include new endpoints' payloads and responses
    - Updated blog tests file
    - Updated README

--- src/lib/blog/Blog.ts
+++ src/lib/blog/Blog.ts
@@ -20,6 +20,64 @@ class Blog extends Resource {
     },
   });
 
+  publish = APIEndpoint<Types.PublishBlogPostPayload, Types.PublishBlogPostResponse>({
+    method: 'post',
+    path: '/api/sites/multiscreen/{site_name}/blog/posts/{post_id}/publish',
+    defaults: {
+      host: 'api.duda.co',
+    },
+  });
+
+  unpublish = APIEndpoint<Types.UnpublishBlogPostPayload, Types.UnpublishBlogPostResponse>({
+    method: 'post',
+    path: '/api/sites/multiscreen/{site_name}/blog/posts/{post_id}/unpublish',
+    defaults: {
+      host: 'api.duda.co',
+    },
+  });
+
+  update = APIEndpoint<Types.UpdateBlogPostPayload, Types.UpdateBlogPostResponse>({
+    method: 'patch',
+    path: '/api/sites/multiscreen/{site_name}/blog/posts/{post_id}',
+    defaults: {
+      host: 'api.duda.co',
+    },
+  });
+
+  list = APIEndpoint<Types.ListBlogPostsPayload, Types.ListBlogPostsResponse>({
+    method: 'get',
+    path: '/api/sites/multiscreen/{site_name}/blog/posts',
+    defaults: {
+      host: 'api.duda.co',
+    },
+    queryParams: {
+      limit: {
+        type: 'number',
+        required: false,
+      },
+      offset: {
+        type: 'number',
+        required: false,
+      },
+    },
+  });
+
+  get = APIEndpoint<Types.GetBlogPostPayload, Types.GetBlogPostResponse>({
+    method: 'get',
+    path: '/api/sites/multiscreen/{site_name}/blog/posts/{post_id}',
+    defaults: {
+      host: 'api.duda.co',
+    },
+  });
+
+  deletePost = APIEndpoint<Types.DeleteBlogPostPayload, Types.DeleteBlogPostResponse>({
+    method: 'delete',
+    path: '/api/sites/multiscreen/{site_name}/blog/posts/{post_id}',
+    defaults: {
+      host: 'api.duda.co',
+    },
+  });
+
   delete = APIEndpoint<Types.DeleteBlogPayload, Types.DeleteBlogResponse>({
     method: 'delete',
     path: '/api/sites/multiscreen/{site_name}/blog',
